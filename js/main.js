ef92c037e274e70d91120c37046c39124d2cb8c2
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-02-20 12:09:45 +0300

    Исправляет Нужно больге функций

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-const getRandomNumber = (minNumber, maxNumber) => {//почему не работает, если без обновления второй раз вставить в консоль браузера?
+const getRandomNumber = (minNumber, maxNumber) => {
   if (minNumber < 0) {
     return('Введите значение больше 0');
   }
   return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
 };
 getRandomNumber(5, 10);
-// посмотрела даже другие Kekstagram на GitHub - другого способа вернуть рандомное число не нашла. Если есть - помоги!!
 
-const checkMaxLenght = (lineNumber, lineMaxLenght) => {
-  const lineLenght = 120;
-  if (lineLenght <= lineMaxLenght) {
-    return true;
-  }
-  return false;
+
+const checkMaxLength = (lineNumber, lineMaxLength) => {
+  const lineLength = 120;
+  return lineLength <= lineMaxLength;
 };
-checkMaxLenght(3, 140);
-//какая-то странная функция. Не правильнее ли вторым параметром написать длину строки и передавать ее при вызове, а переменной сделать максимальную длину? Ведь размер комментария меняется чаще, чем максимальное количество символов? Но в ДЗ указаны именно такие параметры.
+checkMaxLength(3, 140);
+
