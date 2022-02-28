d9d26ac505f8589c5e8af072d401b38867888b1a
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-02-28 12:18:51 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,69 @@
-const getRandomNumber = (minNumber, maxNumber) => {
-  if (minNumber < 0) {
-    return('Введите значение больше 0');
+const checkStringLength = (string, stringMaxLength) => string.length <= stringMaxLength;
+checkStringLength(3, 140);
+
+const getRandomPositiveNumber = (a, b) => {
+  const minNumber = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const maxNumber = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result =  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  return result;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveNumber(0, elements.length - 1)];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const names = [
+  'Василий',
+  'Барсик',
+  'Антонио',
+  'Рудольф',
+  'Иннокентий',
+  'Бонифаций',
+  'Гарфильд',
+  'Арнольд',
+  'Леопольд',
+  'Том',
+  'Феликс',
+  'Тихон'
+];
+
+const minComments = 1;
+const maxComments = 7;
+const arrayNumber = 25;
+const uniqueIdNumber = [];
+
+for (let i = 1; i<=(maxComments*arrayNumber); i++ ) {
+  const uniqueNumber = getRandomPositiveNumber (1, 1000);
+  if (uniqueNumber !== uniqueIdNumber[i]) {
+    uniqueIdNumber.push (uniqueNumber);
   }
-  return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-};
-getRandomNumber(5, 10);
+}
 
+const createComments = () => ({
+  id: getRandomArrayElement(uniqueIdNumber),
+  avatar: `img/avatar-${getRandomPositiveNumber(1, 6)}.svg`,
+  message: getRandomArrayElement(messages),
+  name: getRandomArrayElement(names),
+});
 
-const checkMaxLength = (lineNumber, lineMaxLength) => {
-  const lineLength = 120;
-  return lineLength <= lineMaxLength;
-};
-checkMaxLength(3, 140);
+const photoDescription = [];
+console.log(photoDescription);
 
+for (let i = 1; i <= arrayNumber; i++) {
+  const ArrayComments = Array.from({length:(getRandomPositiveNumber(minComments, maxComments)),}, createComments);
+
+  photoDescription.push({
+    id: i,
+    url: `photo/${i}.jpg`,
+    description: `Фотография ${i}`,
+    likes: getRandomPositiveNumber(15, 200),
+    comments: ArrayComments
+  });
+}
