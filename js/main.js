bc61f4bbe55732f5a7863544d4d0c85c147b7ed2
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-04-06 11:21:48 +0300

    удалить

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 import './api.js';
-import {createGallery} from './data.js';
+import {createGalleryElement, createDefaultGallery, createGallery} from './data.js';
 import {getData} from './api.js';
 import './form.js';
 import './photo-effects.js';
+import {addThumbnailClickHandler} from './fullscreen.js';
 
 
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
 getData((pictures) => {
-  createGallery(pictures);
-});
+  createGalleryElement(pictures);
+  createGallery(pictures)
+  createDefaultGallery(pictures)
+
+
+  //createDefaultGallery(debounce(
+  //() => createGalleryElement(pictures),
+  // 500,
+  //));
+})
+
 
